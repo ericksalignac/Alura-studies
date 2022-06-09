6488a6b806a12dd0a672d43c6802ea1e0388fb2a
ericksalignac <ericksalignac@gmail.com>
2022-06-09 08:55:45 -0300

    Criação, componentização e estilização do app

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../Components/Formulario';
+import Lista from '../Components/Lista';
+import style from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={style.AppStyle}>
+     <Formulario/>
+     <Lista />
+    </div>
+  );
+}
+
+export default App;
