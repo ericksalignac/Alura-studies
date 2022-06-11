51d5636bf0f325e63d392338e390d853e2c5f37c
ericksalignac <ericksalignac@gmail.com>
2022-06-11 17:58:58 -0300

    Implementando adição de novas tarefas

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cronometro from '../Components/Cronometro';
 import Formulario from '../Components/Formulario';
 import Lista from '../Components/Lista';
+import { ITarefa } from '../types/tarefa';
 import style from "./App.module.scss";
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
-     <Formulario/>
-     <Lista />
+     <Formulario setTarefas ={setTarefas}/>
+     <Lista tarefas ={tarefas}/>
      <Cronometro />
     </div>
   );
