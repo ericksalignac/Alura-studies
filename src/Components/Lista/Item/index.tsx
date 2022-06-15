c46c098aa7e51072748180bb07b92baf9eadd86f
ericksalignac <ericksalignac@gmail.com>
2022-06-15 11:25:13 -0300

    Correção na seleção dos itens

--- src/Components/Lista/Item/index.tsx
+++ src/Components/Lista/Item/index.tsx
@@ -1,35 +1,35 @@
-import { ITarefa } from "../../../types/tarefa";
-import style from "../Lista.module.scss";
+import { ITarefa } from '../../../types/tarefa';
+import style from './Item.module.scss';
 
 interface Props extends ITarefa {
-    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
 }
+
 export default function Item(
-    {
-        tarefa,
-        tempo,
-        selecionado,
-        completado,
-        id,
-        selecionaTarefa
-    }: Props) { 
-        console.log('item atual: ', {tarefa, tempo, selecionado, completado, id
-            })
-    return (
-        <li 
-        className={`${style.item} ${selecionado ? style.iitemSelecionado : ''}`} 
-        onClick={() => selecionaTarefa(
-                {
-                tarefa,
-                tempo,
-                selecionado,
-                completado,
-                id
-                }
-            )}
-        >
-        <h3> { tarefa } </h3>
-        <span> {tempo} </span>
+  {
+    tarefa,
+    tempo,
+    selecionado,
+    completado,
+    id,
+    selecionaTarefa
+  }: Props) {
+  console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
+  return (
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+      onClick={() => selecionaTarefa(
+        {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        }
+        )}
+      >
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
     </li>
-    )
+  )
 }
