3335778fa55b9104be6ebb53e6f1a7196a597df2
ericksalignac <ericksalignac@gmail.com>
2022-06-15 15:53:06 -0300

    Aplicação finalizada

--- src/Components/Lista/Item/index.tsx
+++ src/Components/Lista/Item/index.tsx
@@ -17,8 +17,8 @@ export default function Item(
   console.log('item atual: ', { tarefa, tempo, selecionado, completado, id })
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-      onClick={() => selecionaTarefa(
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`}
+      onClick={() => !completado && selecionaTarefa(
         {
           tarefa,
           tempo,
@@ -30,6 +30,10 @@ export default function Item(
       >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && <span 
+      className={style.concluido}
+      aria-Label = "tarefa-completada"
+      ></span>}
     </li>
   )
 }
